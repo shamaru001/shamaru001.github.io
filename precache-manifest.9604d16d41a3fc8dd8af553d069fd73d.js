35e039a908944b69093a33b39e544ddd3417731f
Shamaru Primera <shamaru001@gmail.com>
2019-11-18 01:26:51 -0500

    Updates

---
+++ precache-manifest.9604d16d41a3fc8dd8af553d069fd73d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb36b85d90a8359709711de97088873",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e03b718899464ade1b9f",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cbe33d17d73ac46df099",
+    "url": "/static/css/main.f4f8b721.chunk.css"
+  },
+  {
+    "revision": "e03b718899464ade1b9f",
+    "url": "/static/js/2.3c4000ce.chunk.js"
+  },
+  {
+    "revision": "cbe33d17d73ac46df099",
+    "url": "/static/js/main.9c0eb0ea.chunk.js"
+  },
+  {
+    "revision": "7ee3fc73d32f4c7ff891",
+    "url": "/static/js/runtime-main.c276ed44.js"
+  },
+  {
+    "revision": "c0f33811d3b88782ec4af931a0aac3ee",
+    "url": "/static/media/background_home.c0f33811.jpg"
+  }
+]);
